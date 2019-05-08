4d818e3fcc87c0beda3ec90156bc29d6b3c75659
Alex Langer <aclanger131@gmail.com>
2019-05-08 09:36:52 +0200

    add cards for project section

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Tab, Tabs } from 'react-mdl'
+import {Tab, Tabs, Grid, Cell, Card, CardTitle, CardActions, CardText, CardMenu, Button, IconButton } from 'react-mdl'
 
 class Projects extends Component {
     /*class has a state so we use a constructor */
@@ -12,16 +12,84 @@ class Projects extends Component {
     toggleCategories() {
         if (this.state.activeTab === 0 ) {
             return (
-                <div><h1>hey ho</h1></div>
+                <div className="projects-grid">
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto' }}>
+                    <CardTitle style={{color: 'black', height: '176px', 
+                    background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png) center / cover' }}>
+                    React Project #1
+                    </CardTitle>
+                    <CardText>
+                        Lorem ipsum amet dolor 
+                    </CardText>
+                    <CardActions border>
+                        <Button colored >Github</Button>
+                        <Button colored >Live Demo</Button>
+                        
+                    </CardActions>
+                    <CardMenu style={{color: 'ffff'}}>
+                        <IconButton name="share">Share</IconButton>
+                    </CardMenu>
+                    </Card>
+
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: 'black', height: '176px', 
+                    background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png) center / cover' }}>
+                    React Project #2
+                    </CardTitle>
+                    <CardText>
+                        li la lu 
+                    </CardText>
+                    <CardActions border>
+                        <Button colored >Github</Button>
+                        <Button colored >Live Demo</Button>
+                        
+                    </CardActions>
+                    <CardMenu style={{color: 'ffff'}}>
+                        <IconButton name="share">Share</IconButton>
+                    </CardMenu>
+                    </Card>
+                    </div>
             )
         } else if (this.state.activeTab === 1 ){
             return (
-                <div>angular stuff</div>
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: 'black', height: '176px', 
+                    background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png) center / cover' }}>
+                    JavaScript Project #1
+                    </CardTitle>
+                    <CardText>
+                        Lorem ipsum amet dolor 
+                    </CardText>
+                    <CardActions border>
+                        <Button colored >Github</Button>
+                        <Button colored >Live Demo</Button>
+                        
+                    </CardActions>
+                    <CardMenu style={{color: 'ffff'}}>
+                        <IconButton name="share">Share</IconButton>
+                    </CardMenu>
+                    </Card>
             )
         }
         else if (this.state.activeTab === 2 ){
             return (
-                <div>html dingens</div>
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: 'black', height: '176px', 
+                    background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png) center / cover' }}>
+                    HTML Project #1
+                    </CardTitle>
+                    <CardText>
+                        Lorem ipsum amet dolor 
+                    </CardText>
+                    <CardActions border>
+                        <Button colored >Github</Button>
+                        <Button colored >Live Demo</Button>
+                        
+                    </CardActions>
+                    <CardMenu style={{color: 'ffff'}}>
+                        <IconButton name="share">Share</IconButton>
+                    </CardMenu>
+                    </Card>
             )
         }
     }
@@ -35,9 +103,16 @@ class Projects extends Component {
                     <Tab>Html</Tab>
                 </Tabs>
 
-                <section className="projects-grid">
-                    {this.toggleCategories()}
-                </section>
+                
+                    <Grid >
+                    <Cell col={12}>
+                        <div className="content">
+                        {this.toggleCategories()}
+                        </div>
+                    </Cell>
+                    </Grid>
+                    
+               
             </div>
         )
     }
